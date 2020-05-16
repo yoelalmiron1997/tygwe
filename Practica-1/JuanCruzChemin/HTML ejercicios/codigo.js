00852993dcf3dd938f39b12d8ba935b40ca6e803
Juan Cruz Chemin <juancruzcheminv@users.noreply.github.com>
2020-05-16 20:04:42 -0300

    Nueva Carpeta

---
+++ Practica-1/JuanCruzChemin/HTML ejercicios/codigo.js
@@ -0,0 +1,6 @@
+
+var mensaje = "Hola Mundo! Que fácil es incluir \'comillas simples\' y \"comillas dobles\"" 
+alert(mensaje);
+
+// Despues del primer mensaje, se muestra otro mensaje seguido
+alert("Soy el primer script");
