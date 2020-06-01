5271b708c2b5344d37e55bd94b0bf311334f8626
FlorenciaFried <florencia.d.fried@gmail.com>
2020-05-31 21:55:09 -0300

    Cambie mi carpeta de Practica-1 a TP1

---
+++ TP-1/FlorenciaFried/app-tp1/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Sidebar></Sidebar>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Sidebar,
+    Footer,
+  }
+}
+</script>
+
+<style>
+</style>
