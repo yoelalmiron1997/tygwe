d9d050926017125f95249ad3b6047b7599d37691
Luciano Garcia Montes <lugarciamontes@gmail.com>
2020-06-03 18:06:14 -0300

    TP terminado. Video renombrado, 2da API funcionando, CSS ordenado.

--- TP-1/LucianoGarciaMontes/js/codigo.js
+++ TP-1/LucianoGarciaMontes/js/codigo.js
@@ -11,19 +11,59 @@ function frase(){
     $("#fraseAleatoria").text(quotes[numeroFrase]);
     $("#fraseAleatoria").attr("class", "frase"+numeroFrase);
     $("#fraseAleatoria").css("visibility","visible");
-}
+};
+
+function nuevaFrase(){
+    var numFrase = Math.floor(Math.random()*(quotes.length));
+    $("#fraseAleatoria").text(quotes[numFrase]);
+    $("#fraseAleatoria").attr("class", "frase"+numFrase);
+    $("#fraseAleatoria").css("visibility","visible");
+};
 
 $(document).ready(function(){
+
+    $(".coronavirus").hide();
     frase();
 
+    $("#botonperro").on("click",function(){
+        $(".coronavirus").hide();
+        $(".fotoperro").show();
+    });
+
+    $("#botoncorona").on("click",function(){
+        $(".fotoperro").hide();
+        $(".fotoperro").empty();
+        $(".coronavirus").show();
+    });
+
     const link = "https://cors-anywhere.herokuapp.com/http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true"
-    $("#botonShibe").click(function(){
+    $("#botonperro").click(function(){
         $.ajax({
             url: link,
             type: "GET",
-            success: function(result){
-                console.log(result);
-                $("#fotoshibe").html('<img src="' + result + '" />');
+            success: function(imagen){
+                console.log(imagen);
+                $(".fotoperro").html('<img src="' + imagen + '" />');
+            },
+            error: function(error){
+                console.log(error)
+            }
+        });
+    });
+
+    const link2 = "https://api.covid19api.com/summary"
+    $("#botoncorona").click(function(){
+        $.ajax({
+            url: link2,
+            type: "GET",
+            success: function(resultados){
+                console.log(resultados);
+                $("#confMundo").html(resultados.Global.TotalConfirmed);
+                $("#muerMundo").html(resultados.Global.TotalDeaths);
+                $("#recMundo").html(resultados.Global.TotalRecovered);
+                $("#confArg").html(resultados.Countries[6].TotalConfirmed);
+                $("#muerArg").html(resultados.Countries[6].TotalDeaths);
+                $("#recArg").html(resultados.Countries[6].TotalRecovered);
             },
             error: function(error){
                 console.log(error)
