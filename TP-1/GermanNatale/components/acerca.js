8c843529fcaa1414e6f61352dfd9ca65a5a9ea34
German Natale <germannatale@gmail.com>
2020-06-04 17:17:51 -0300

    Pseudo router con VUE components

---
+++ TP-1/GermanNatale/components/acerca.js
@@ -0,0 +1,9 @@
+Vue.component('acerca',{
+    template: //html 
+            `
+            <div>    
+                <h1>Hola</h1>
+                <h2>Segundo Dato</h2>
+            </div>
+            `
+})
